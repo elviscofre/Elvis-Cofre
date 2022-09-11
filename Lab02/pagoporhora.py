e9443ff23e155080a54af552d8b8ebef3f6f5272
elviscofre <elvis.cofre@est.zamorano.edu>
2022-09-11 14:25:50 -0600

    Lab02

--- Lab02/pagoporhora.py
+++ Lab02/pagoporhora.py
@@ -6,7 +6,7 @@ valorporhora = input("valor por hora? ")
 # se utiliza la conversión de tipo a int
 # sino se hace la conversion existira error porque trata de multiplicar strings
 total = int(horas) * int(valorporhora)
-print("Usted recibira un total de12:")
+print("Usted recibira un total de:")
 print(total)
 
 # solo falta imprimir el tipo
